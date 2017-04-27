50109abecb262ffb8efbe4463b6fb74a37647958
Jonas Scherer <jonas@matilhaestud.io>
2017-04-27 18:11:28 -0300

    scaffolding

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
+  resources :match_criteria
+  resources :match_categories
  	#root "pages#home", as: 'home'
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,9 +11,26 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170426194744) do
+ActiveRecord::Schema.define(version: 20170427211028) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+  create_table "match_categories", force: :cascade do |t|
+    t.integer  "match_category_id"
+    t.string   "name"
+    t.datetime "created_at",        null: false
+    t.datetime "updated_at",        null: false
+  end
+
+  create_table "match_criteria", force: :cascade do |t|
+    t.string   "title"
+    t.integer  "match_category_id"
+    t.datetime "created_at",        null: false
+    t.datetime "updated_at",        null: false
+  end
+
+  add_index "match_criteria", ["match_category_id"], name: "index_match_criteria_on_match_category_id", using: :btree
+
+  add_foreign_key "match_criteria", "match_categories"
 end
