4145f9c23ddfc8dd6cc05a744e7cac2fe1e4398a
Jonas Scherer <jonas@matilhaestud.io>
2017-04-28 15:39:40 -0300

    adding scaffolding

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
+  resources :criteria
+  resources :categories
  	#root "pages#home", as: 'home'
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,9 +11,25 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170426194744) do
+ActiveRecord::Schema.define(version: 20170428183224) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+  create_table "categories", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "criteria", force: :cascade do |t|
+    t.string   "name"
+    t.integer  "category_id", null: false
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
+
+  add_index "criteria", ["category_id"], name: "index_criteria_on_category_id", using: :btree
+
+  add_foreign_key "criteria", "categories"
 end
