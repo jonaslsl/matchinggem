5863dafbae388a93a73c7ee9ef009e0541f9d008
Jonas Scherer <jonas@matilhaestud.io>
2017-05-05 15:37:01 -0300

    reseting schemas

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,24 +1,3 @@
-c1 = Category.create(name: "Personal Info")
-c2 = Category.create(name: "Music")
-c3 = Category.create(name: "Cinema")
-c4 = Category.create(name: "Hobbies")
-c5 = Category.create(name: "Career")
-
-Criterium.create(category_id: c1.id, name: "Age")
-Criterium.create(category_id: c1.id, name: "Hair Style")
-Criterium.create(category_id: c1.id, name: "Favorite Color")
-
-Criterium.create(category_id: c2.id, name: "Genders")
-Criterium.create(category_id: c2.id, name: "Bands")
-
-Criterium.create(category_id: c3.id, name: "Genders")
-Criterium.create(category_id: c3.id, name: "Movies")
-
-Criterium.create(category_id: c4.id, name: "Favorite sport")
-
-Criterium.create(category_id: c5.id, name: "Job title")
-Criterium.create(category_id: c5.id, name: "Years of experience")
-Criterium.create(category_id: c5.id, name: "Job description")
 
 
 
